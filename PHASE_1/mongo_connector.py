6b88931472f354370e0e56ca69738457b023a40b
Tiana Douroudis <tdouroudis@gmail.com>
2022-03-10 22:08:25 +1100

    database set up

--- PHASE_1/mongo_connector.py
+++ PHASE_1/mongo_connector.py
@@ -1,21 +1,13 @@
-
-import ssl
-
-try:
-    _create_unverified_https_context = ssl._create_unverified_context
-except AttributeError:
-    pass
-else:
-    ssl._create_default_https_context = _create_unverified_https_context
-
 from pymongo import MongoClient
 
 cluster = MongoClient(
     "mongodb+srv://EpicDemic:EpicDemic123!@epicdemic.ul8sw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 
-db = cluster["epicdemic"]
+db = cluster["epicdemic_db"]
+
+# Accessing diseases collection
 collection = db["diseases"]
 
-post = {"_id": 0, "name": "zika"}
-
-collection.insert_one(post)
+# EXAMPLE: HOW TO ADD A DISEASE TO DATABASE
+# post = {"_id": 0, "name": "zika"}
+# collection.insert_one(post)
