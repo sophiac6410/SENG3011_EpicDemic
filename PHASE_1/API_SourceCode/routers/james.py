ebc213782be91b8842485d3946c84d8d9047be10
James Ji <jamesx.ji@gmail.com>
2022-03-13 20:40:05 +1100

    checkpoint

--- PHASE_1/API_SourceCode/routers/james.py
+++ PHASE_1/API_SourceCode/routers/james.py
@@ -1,10 +1,42 @@
+from typing import List
 from fastapi import APIRouter
-from database import articles_col
+from database import articles_col, reports_col, diseases_col
 import re
 
 router = APIRouter(
     prefix='/james'
 )
+
+@router.post("/reports/ids")
+async def get_reports(
+    report_ids: List[int]):
+    report_docs = list(reports_col.find(
+        {"report_id":{"$in":report_ids}},
+        {"_id": False}
+    ))
+    
+    reports = {}
+    disease_ids = set()
+    
+    for doc in report_docs:
+        reports["report_id"] = doc
+        
+        # Get all the disease ids so we can look them all up in one efficient query
+        for disease_id in doc["diseases"]:
+            disease_ids.add(disease_id)
+
+    disease_docs = list(diseases_col.find(
+        {"_id":{"$in":disease_ids}}
+    ))
+
+
+    return {
+        "status": 200,
+        "data": {
+            "reports": reports
+        }
+    }
+
 
 @router.get("/test/{key_term}")
 async def get_test(
