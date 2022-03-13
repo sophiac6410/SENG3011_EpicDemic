14a52d6420fb08b7360d5f505ccc1234b0d85099
Sophia <z5312941@ad.unsw.edu.au>
2022-03-14 09:55:38 +1100

    tidied up docs, added schemas and examples

--- PHASE_1/API_SourceCode/routers/sophia.py
+++ PHASE_1/API_SourceCode/routers/sophia.py
@@ -11,43 +11,93 @@ router = APIRouter(
     prefix='/sophia'
 )
 
-@router.get('/healthcheck', status_code=status.HTTP_200_OK)
+class HealthCheck(BaseModel):
+	healthcheck: str
+
+	class Config:
+		schema_extra = {
+			"example": {
+				"healthcheck": "Everything OK!"
+			}
+		}
+@router.get('/healthcheck', status_code=status.HTTP_200_OK, response_model=HealthCheck)
 def perform_healthcheck():
     return {'healthcheck': 'Everything OK!'}
 
-@router.get("/test/{key_term}", status_code=status.HTTP_200_OK)
-async def get_test(
-	key_term: str
-):
-    documents = list(articles_col.find(
-		{"headline": re.compile(key_term, re.IGNORECASE)}, 
-		{"_id": False}))
+# @router.get("/test/{key_term}", status_code=status.HTTP_200_OK)
+# async def get_test(
+# 	key_term: str
+# ):
+#     documents = list(articles_col.find(
+# 		{"headline": re.compile(key_term, re.IGNORECASE)}, 
+# 		{"_id": False}))
 
-    print(documents[0]["date_of_publication"])
-    print(documents[0]["date_of_publication"] > documents[1]["date_of_publication"])
+#     print(documents[0]["date_of_publication"])
+#     print(documents[0]["date_of_publication"] > documents[1]["date_of_publication"])
     
-    result = []
-    for document in documents:
-        result.append(document["article_id"])
+#     result = []
+#     for document in documents:
+#         result.append(document["article_id"])
 
-	# for document in documents:
-	# 	print(document)
+# 	# for document in documents:
+# 	# 	print(document)
 
-    return {
-		"status": 200,
-		"data": {
-			"article_ids": result
-		}
-	}
+#     return {
+# 		"status": 200,
+# 		"data": {
+# 			"article_ids": result
+# 		}
+# 	}
 
 ############## GET ARTICLES BY QUERY ###############
-# class Article(BaseModel):
-# 	article_id: int
-# 	url: int
-# 	date_of_publication: str
-# 	headline: str
-# 	main_text: str
-# 	reports: List(int)
+class ReportIds(BaseModel):
+	report_id: int
+
+	class Config:
+		schema_extra = {
+            "example": {
+                "report_id": 0
+            }
+        }
+
+class Article(BaseModel):
+	article_id: int
+	url: str
+	date_of_publication: datetime
+	headline: str
+	main_text: str
+	reports: List[ReportIds]
+
+	class Config:
+		schema_extra = {
+            "example": {
+                "article_id": 8701909,
+				"url": "https://promedmail.org/promed-post/?id=8701909",
+				"date_of_publication": "2022-03-10T02:08:51",
+				"headline": "PRO/AH/EDR> Chronic wasting disease - North America (02): (USA) deer",
+				"main_text": "The Iowa Department of Natural Resources reports 36 positive chronic wasting disease (CWD) tests from some 5000 deer samples this hunting season.<br/><br/>The DNR's Tyler Harms, who oversees the deer management program, says 2 new counties were added to the list of counties in which CWD has been detected in the wild. Greene County in central Iowa and Fremont County in southwest Iowa brings the total number of counties to 12.",
+				"reports": [{"report_id": 0}, {"report_id": 1}]
+            }
+        }
+
+class ArticleQueryResponse(BaseModel):
+	start_range: int
+	end_range: int
+	articles: List[Article]
+
+	class Config:
+		schema_extra = {
+            "example": {
+				"start_range": 1,
+				"end_range": 10,
+				"articles": [{"article_id": 8701909,
+				"url": "https://promedmail.org/promed-post/?id=8701909",
+				"date_of_publication": "2022-03-10T02:08:51",
+				"headline": "PRO/AH/EDR> Chronic wasting disease - North America (02): (USA) deer",
+				"main_text": "The Iowa Department of Natural Resources reports 36 positive chronic wasting disease (CWD) tests from some 5000 deer samples this hunting season.<br/><br/>The DNR's Tyler Harms, who oversees the deer management program, says 2 new counties were added to the list of counties in which CWD has been detected in the wild. Greene County in central Iowa and Fremont County in southwest Iowa brings the total number of counties to 12.",
+				"reports": [{"report_id": 0}]}]
+			}
+		}
 
 description_start_date = "Requests articles published after the start_date. Format: 'yyyy-MM-ddTHH:mm:ss'"
 description_end_date = "Requests articles published before the end_date. Format: 'yyyy-MM-ddTHH:mm:ss'"
@@ -56,8 +106,8 @@ description_timezone = "The timezone of the start_date and end_date. Must be in 
 description_start_range = "Specifies the position of the article to start from."
 description_end_range = "Specifies the position of the last article to return. If the position is out of range, the API will return up to the last article."
 
-@router.get("/articles", status_code=status.HTTP_200_OK)
-async def get_articles(
+@router.get("/articles", status_code=status.HTTP_200_OK, response_model=ArticleQueryResponse)
+async def get_articles_by_query(
 	*, # including this allows parameters to be defined in any order
 	start_date: datetime = Query(
 		..., # no default, is required
@@ -110,11 +160,7 @@ async def get_articles(
 			"headline": {"$in":[re.compile(x, re.IGNORECASE) for x in terms_list]}
 			}
 		},
-		{"$project": {
-			"diseases": False,
-			"_id": False,
-			}
-		},
+		{"$project": {"diseases": 0, "_id": 0}},
 		{"$skip":start_range-1},
 		{"$limit": end_range-start_range+1},
 		{"$sort": {"date_of_publication": -1}},
@@ -123,8 +169,8 @@ async def get_articles(
 			"localField": "article_id",
 			"foreignField": "article_id",
 			"pipeline": [
-				{"$group": {"_id": None, "reports": {"$push": "$report_id"}}},
-				{"$project": {"_id": 0, "reports": 1}}
+				# {"$group": {"_id": None, "reports": {"$push": "$report_id"}}},
+				{"$project": {"_id": 0, "report_id": 1}}
 			],
 			"as": "reports"
 			}
@@ -132,19 +178,19 @@ async def get_articles(
 	]))
 	
 	return {
-		"status": 200,
-		"data": {
-			"start_range": start_range,
-			"end_range": end_range,
-			"articles": articles
-		}
+		"start_range": start_range,
+		"end_range": end_range,
+		"articles": articles
 	}
 
 
 
 ############## GET ARTICLES BY IDS ###############
-@router.get("/articles/ids", status_code=status.HTTP_200_OK)
-async def get_article_ids(
+class ArticleIdResponse(BaseModel):
+	articles: List[Article]
+
+@router.get("/articles/ids", status_code=status.HTTP_200_OK, response_model=ArticleIdResponse)
+async def get_articles_by_ids(
 	article_ids: str = Query(
 		...,
 		description="The articles' unique id's, separated by commas.",
@@ -154,25 +200,19 @@ async def get_article_ids(
 	id_list = [int(i) for i in article_ids.split(",")]
 	articles = list(articles_col.aggregate([
 		{"$match": {"article_id": {"$in": id_list}}},
-		{"$project": {"_id": False, "diseases": False}},
+		{"$project": {"diseases": False, "_id": False}},
 		{"$lookup": {
 			"from": "Reports",
 			"localField": "article_id",
 			"foreignField": "article_id",
 			"pipeline": [
-				{"$group": {"_id": None, "reports": {"$push": "$report_id"}}},
-				{"$project": {"_id": 0, "reports": 1}}
+				# {"$group": {"_id": None, "reports": {"$push": "$report_id"}}},
+				{"$project": {"_id": 0, "report_id": 1}}
 			],
 			"as": "reports"
 			}
 		}
 	]))
-	status = 200
-	if articles == []:
-		status = 204
 	return {
-		"status": status,
-		"data": {
-			"articles": articles
-		}
+		"articles": articles
 	}
