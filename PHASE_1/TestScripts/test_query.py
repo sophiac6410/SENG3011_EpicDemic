a0e22d8fb2e5e1ff89f538cb65538b309328e2df
James Ji <jamesx.ji@gmail.com>
2022-03-18 01:30:39 +1100

    done test???

--- PHASE_1/TestScripts/test_query.py
+++ PHASE_1/TestScripts/test_query.py
@@ -37,3 +37,60 @@ def test_query_start_end_date_article_ids():
         }
     }
 
+def test_query_start_end_date_article_ids_location_ids():
+    start_date = datetime.fromisoformat("2021-01-01T10:10:10")
+    end_date = datetime.fromisoformat("2023-01-01T10:10:10")
+    article_ids = [1, 2, 3]
+    location_ids = [5, 6, 7]
+
+    query = generate_query(start_date, end_date, article_ids, location_ids)
+    
+    assert len(query) == 3
+    assert query[0] == {
+        "event_date": {
+            "$gte": start_date,
+            "$lte": end_date,
+        }
+    }
+    assert query[1] == {
+        "article_id": {
+            "$in": [1, 2, 3]
+        }
+    }
+    assert query[2] == {
+        "locations": {
+            "$in": [5, 6, 7]
+        }
+    }
+    
+def test_query_start_end_date_article_ids_location_ids_disease_ids():
+    start_date = datetime.fromisoformat("2021-01-01T10:10:10")
+    end_date = datetime.fromisoformat("2023-01-01T10:10:10")
+    article_ids = [1, 2, 3]
+    location_ids = [5, 6, 7]
+    disease_ids = [9, 10, 11]
+
+    query = generate_query(start_date, end_date, article_ids, location_ids, disease_ids)
+    
+    assert len(query) == 4
+    assert query[0] == {
+        "event_date": {
+            "$gte": start_date,
+            "$lte": end_date,
+        }
+    }
+    assert query[1] == {
+        "article_id": {
+            "$in": [1, 2, 3]
+        }
+    }
+    assert query[2] == {
+        "locations": {
+            "$in": [5, 6, 7]
+        }
+    }
+    assert query[3] == {
+        "diseases": {
+            "$in": [9, 10, 11]
+        }
+    }
