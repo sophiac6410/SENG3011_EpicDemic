d79f9fd47a2d9685daf86472fd748f80b0fe7010
z5259266 <r.espiritu@student.unsw.edu.au>
2022-03-27 01:43:02 +1100

    need to do diff divs

---
+++ PHASE_2/Application_SourceCode/epicdemic/src/components/NewsBar.js
@@ -0,0 +1,60 @@
+import React from "react";
+import NewsCard from "./NewsCard";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import "../styles/Home.css"
+import { Row } from "react-bootstrap";
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+
+const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 2000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
+
+class NewsBar extends React.Component {
+  render() {
+    return(
+      <Row className="mt-2">
+        <div className="sub-title mb-2">GLOBAL NEWS</div>
+        <div className="news-carousel">
+          <Carousel 
+            responsive={responsive} 
+            containerClass="news-carousel"
+            autoPlay={false}
+            arrows={false}
+            shouldResetAutoplay={false}
+            itemClass="location-card"
+            centerMode={true}
+          >
+              <NewsCard></NewsCard>
+              <NewsCard></NewsCard>
+              <NewsCard></NewsCard>
+              <NewsCard></NewsCard>
+              <NewsCard></NewsCard>
+              <NewsCard></NewsCard>
+              <NewsCard></NewsCard>
+              <NewsCard></NewsCard>
+          </Carousel>
+        </div>
+      </Row>
+    )
+  }
+}
+
+export default NewsBar
--- PHASE_2/Application_SourceCode/epicdemic/src/pages/home.js
+++ PHASE_2/Application_SourceCode/epicdemic/src/pages/home.js
@@ -1,11 +1,13 @@
 import {Container} from "react-bootstrap";
 import LocationBar from "../components/LocationBar";
+import NewsBar from "../components/NewsBar";
 import Search from "../components/Search";
 
 function Home() {
   return (
       <Container style={{"background-color": "#3F3CB0"}}>
         <Search></Search>
+        <NewsBar></NewsBar>
         <LocationBar></LocationBar> 
       </Container>
   );
