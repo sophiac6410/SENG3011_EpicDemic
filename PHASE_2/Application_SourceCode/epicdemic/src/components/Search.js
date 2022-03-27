45f267fc58a2b0395ea559fb95dec7849ab129f2
z5259266 <r.espiritu@student.unsw.edu.au>
2022-03-28 00:31:56 +1100

    search looking ok

--- PHASE_2/Application_SourceCode/epicdemic/src/components/Search.js
+++ PHASE_2/Application_SourceCode/epicdemic/src/components/Search.js
@@ -1,25 +1,42 @@
 import {InputGroup, FormControl, Row, Col } from "react-bootstrap";
+import CountrySelect from "./CountrySearchBox";
+import "react-multi-carousel/lib/styles.css";
+import SearchIcon from '@mui/icons-material/Search';
 
 function Search() {
     return(
       <Row className="p-4" style={{"background-color": "#726FE7"}}>
       <Col>
-        <InputGroup>
+        {/* <InputGroup>
           <InputGroup.Text id="from-search-icon">a</InputGroup.Text>
           <FormControl
             placeholder="From"
             aria-describedby="from-search-icon"
           />
-        </InputGroup>
+        </InputGroup> */}
+        <div className="search-bar-text">From:</div>
+        <div className="searchfield-container">
+          <div className="search-icon">
+            <SearchIcon fontSize="large"></SearchIcon>
+          </div>
+          <CountrySelect fieldLabel={"Choose a country"}></CountrySelect>
+        </div>
       </Col>
       <Col>
-        <InputGroup>
+        {/* <InputGroup>
           <InputGroup.Text id="dest-search-icon">a</InputGroup.Text>
           <FormControl
             placeholder="Destination"
             aria-describedby="dest-search-icon"
           />
-        </InputGroup>
+        </InputGroup> */}
+        <div className="search-bar-text">To:</div>
+        <div className="searchfield-container">
+          <div className="search-icon">
+            <SearchIcon fontSize="large"></SearchIcon>
+          </div>
+          <CountrySelect fieldLabel={"Choose a country"}></CountrySelect>
+        </div>
       </Col>
     </Row>
     )
