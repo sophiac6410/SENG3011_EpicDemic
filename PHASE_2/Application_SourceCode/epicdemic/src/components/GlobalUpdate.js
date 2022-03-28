47f0257da7838f37759ac878b9191ebe1e78dee1
Christina Xie <xxychristina@gmail.com>
2022-03-28 23:28:51 +1100

    rewrite class to function

--- PHASE_2/Application_SourceCode/epicdemic/src/components/GlobalUpdate.js
+++ PHASE_2/Application_SourceCode/epicdemic/src/components/GlobalUpdate.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Switch from "react-switch";
 import { Row, Form, Col } from "react-bootstrap";
 import '../styles/Home.css'
@@ -47,71 +47,61 @@ function UpdateBox(props) {
   }
 }
 
-class GlobalUpdate extends React.Component{
-  constructor() {
-    super();
-    this.state = { checked: false };
-    this.handleChange = this.handleChange.bind(this);
-  }
+function GlobalUpdate(){
+  const [checked, setChecked] = useState(false);
 
-  handleChange(checked) {
-    this.setState({ checked });
-  }
-
-  render() {
-    return(
-      <Col className="bg-lightpurple pt-4 pb-4 mt-3">
-        <Row className="pe-5">
-          <Col md={9}>
-            <div className="sub-title mb-2">GLOBAL COVID UPDATES</div>
-          </Col>
-          <Col className="align-self-center">
-            <Row>
-              <Col>
-                <Row className="align-self-center justify-content-end pe-3" style={{"font-size": "20px", "color": "white", "font-weight": "bold"}}>Cases</Row>
-              </Col>
-              <Col>
-                <Switch
-                    checked={this.state.checked}
-                    onChange={this.handleChange}
-                    onColor="#86d3ff"
-                    onHandleColor="#2693e6"
-                    handleDiameter={30}
-                    uncheckedIcon={false}
-                    checkedIcon={false}
-                    height={20}
-                    width={48}
-                    className="react-switch align-self-center"
-                    id="material-switch"
-                  />
-              </Col>
-              <Col>
-                <Row className="align-self-center justify-content-start" style={{"font-size": "20px", "color": "white", "font-weight": "bold"}}>Vaccines</Row>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-        <div>
-          <Carousel
-            responsive={responsive} 
-            autoPlay={false}
-            arrows={true}
-            shouldResetAutoplay={false}
-            centerMode={true}
-          >
-            <Col className="mt-3">
-              <UpdateBox number="481M" text="total cases" color="blue"></UpdateBox>
-              <UpdateBox number="6.11M" text="deaths" color="white"></UpdateBox>
-              <UpdateBox number="1.7M" text="daily cases" color="blue"></UpdateBox>
+  return(
+    <Col className="bg-lightpurple pt-4 pb-4 mt-3">
+      <Row className="pe-5">
+        <Col md={9}>
+          <div className="sub-title mb-2">GLOBAL COVID UPDATES</div>
+        </Col>
+        <Col className="align-self-center">
+          <Row>
+            <Col>
+              <Row className="align-self-center justify-content-end pe-3" style={{"font-size": "20px", "color": "white", "font-weight": "bold"}}>Cases</Row>
             </Col>
-            <div>
-              <img src={covidMap} width="950px" height="425px" className="p-3"></img>
-            </div>
-          </Carousel>
-        </div>
-      </Col>
-    )
-  }
+            <Col>
+              <Switch
+                  checked={checked}
+                  onChange={setChecked(!checked)}
+                  onColor="#86d3ff"
+                  onHandleColor="#2693e6"
+                  handleDiameter={30}
+                  uncheckedIcon={false}
+                  checkedIcon={false}
+                  height={20}
+                  width={48}
+                  className="react-switch align-self-center"
+                  id="material-switch"
+                />
+            </Col>
+            <Col>
+              <Row className="align-self-center justify-content-start" style={{"font-size": "20px", "color": "white", "font-weight": "bold"}}>Vaccines</Row>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      <div>
+        <Carousel
+          responsive={responsive} 
+          autoPlay={false}
+          arrows={true}
+          shouldResetAutoplay={false}
+          centerMode={true}
+        >
+          <Col className="mt-3">
+            <UpdateBox number="481M" text="total cases" color="blue"></UpdateBox>
+            <UpdateBox number="6.11M" text="deaths" color="white"></UpdateBox>
+            <UpdateBox number="1.7M" text="daily cases" color="blue"></UpdateBox>
+          </Col>
+          <div>
+            <img src={covidMap} width="950px" height="425px" className="p-3"></img>
+          </div>
+        </Carousel>
+      </div>
+    </Col>
+  )
 }
 
 export default GlobalUpdate
