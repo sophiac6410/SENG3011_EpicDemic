b161c97510ae9c21aa247df91c9cf797a08c6120
Christina Xie <xxychristina@gmail.com>
2022-04-01 22:33:39 +1100

    router setup

--- PHASE_2/Application_SourceCode/epicdemic/src/App.js
+++ PHASE_2/Application_SourceCode/epicdemic/src/App.js
@@ -1,14 +1,25 @@
 
 import NavbarComp from './components/Navbar'
 import Home from './pages/home'
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link,
+    BrowserRouter
+  } from "react-router-dom";
+
 function App() {
     return(
-        <div>
-            <NavbarComp></NavbarComp>
-            <div>
-                <Home></Home>
-            </div>
-        </div>
+      <div>
+        <NavbarComp></NavbarComp>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home></Home>}>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
     )
 }
 
