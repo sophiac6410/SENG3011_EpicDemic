e3a6700407ccc48da5848a9c0db9c4e5a6db2a73
rovielynesp <rovielynespiritu@outlook.com>
2022-04-17 22:22:05 +1000

    fixed typos

--- PHASE_1/API_SourceCode/models/updateModels.py
+++ PHASE_1/API_SourceCode/models/updateModels.py
@@ -4,15 +4,15 @@ from typing import List
 from models import baseModels
 
 class Update(BaseModel):
-    location_id: str = Field(..., description="The ISO code of the location for the update", example="AT"),
-    country: str = Field(..., description="The country name that the update refers to", example="Austria"),
-    text: str = Field(..., description="The description of the update", example="Wearing FFP2 face masks is mandatory in all indoor public spaces na..."),
-    date: datetime = Field(..., description="The date of the update", example="2022-03-02"),
+    location_id: str = Field(..., description="The ISO code of the location for the update", example="AT")
+    country: str = Field(..., description="The country name that the update refers to", example="Austria")
+    text: str = Field(..., description="The description of the update", example="Wearing FFP2 face masks is mandatory in all indoor public spaces na...")
+    date: datetime = Field(..., description="The date of the update", example="2022-03-02")
     collection_type: str = Field(..., description="The category of the update. Possible values are 'quarantine', 'testing', 'document', 'mask', 'tracing', 'policies', 'Others'", example="mask")
 
 class UpdateResponse(BaseModel):
-    start: int = Field(..., description="The starting index for the returned updates", example=1),
-    end: int = Field(..., description="The end index for the returned updates", example=5),
+    start: int = Field(..., description="The starting index for the returned updates", example=1)
+    end: int = Field(..., description="The end index for the returned updates", example=5)
     updates: List[Update] = Field(..., description="A list of the updates")
 
 class Response(baseModels.Response):
