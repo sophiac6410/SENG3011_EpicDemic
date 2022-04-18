11c7a566eebcb874e866ed6720dcc5f8aaee1651
rovielynesp <rovielynespiritu@outlook.com>
2022-04-18 21:30:59 +1000

    fixed api err

--- PHASE_1/API_SourceCode/routers/locations.py
+++ PHASE_1/API_SourceCode/routers/locations.py
@@ -74,34 +74,6 @@ async def get_location_by_id(
 	return baseModels.createResponse(True, 200, data)
 
 
-
-
-############## GET covid cases on all available countries ###############
-@router.get("/covidcases", status_code=status.HTTP_200_OK, response_model=locationModels.LocationCovidResponse)
-async def get_covid_cases():
-
-	data = list(diseaseLocations_col.find())
-	cases_per_country = []
-
-	for country in data:
-		# print(f"-- {country['location']} --")
-		loc_data = list(locations_col.find({'_id': country['location']}))
-		# print(loc_data)
-		if len(loc_data) > 0:
-			loc_data = loc_data[0]
-			cases_per_country.append({
-				'country': loc_data['country'],
-				'longitude': loc_data['longitude'],
-				'latitude': loc_data['latitude'],
-				'cases': country['cases']['confirmed']
-			})
-
-
-	return baseModels.createResponse(True, 200, {
-		"cases_per_country": cases_per_country
-	})
-
-
 ############## GET SAEFTY SCORE OF A COUNTRY ###############
 @router.get("/{id}/safety", status_code=status.HTTP_200_OK, response_model=locationModels.LocationSafetyResponse)
 async def get_location_by_id(
