332755bff7b764ca9e1c6d74fc2fe879a1ba1c97
Sophia <z5312941@ad.unsw.edu.au>
2022-05-14 01:34:33 +1000

    add item modal done, no api calls yet

--- PHASE_2/Application_SourceCode/epicdemic/src/components/Planner/ChecklistModal.js
+++ PHASE_2/Application_SourceCode/epicdemic/src/components/Planner/ChecklistModal.js
@@ -9,15 +9,14 @@ import CloseIcon from '@mui/icons-material/Close';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import { TealBotton } from '../../styles/Button';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import TextField from '@mui/material/TextField';
 
-const styleThree = {
+const mainModal = {
   position: 'absolute',
   top: '50%',
   left: '50%',
@@ -32,8 +31,24 @@ const styleThree = {
   backgroundColor: "#F4FBFF",
   display: "flex",
   flexDirection: "column",
-  paddingBottom: "80px",
 };
+
+const popUp = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: "45vw",
+  minWidth: '300px',
+  height: "60vh",
+  bgcolor: 'background.paper',
+  borderRadius: "30px",
+  boxShadow: 24,
+  p: 4,
+  backgroundColor: "white",
+  display: "flex",
+  flexDirection: "column",
+}
 
 function ChecklistItem({item}) {
   const [checked, setChecked] = React.useState(item.checked);
@@ -105,7 +120,7 @@ function ChecklistModal({checklist, city}) {
       scroll='body'
       aria-labelledby="checklist-dialog"
     >
-      <Box sx={styleThree}>
+      <Box sx={mainModal}>
         <div style={{display: "flex", justifyContent: "end", flexDirection: "row", alignItems: "center"}}>
           <IconButton onClick={handleClose}>
             <CloseIcon color='teal' fontSize="small"></CloseIcon>
@@ -115,15 +130,76 @@ function ChecklistModal({checklist, city}) {
         <div style={{justifyContent: "center", flexDirection: "row", alignItems: "center"}}>
           <Typography variant="heading2" className="color-dark-teal" sx={{textAlign: 'center', lineHeight: 4}}>{city.city_name}, {city.country_name}</Typography>
         </div>
-        <div className="border-radius-med" style={{overflow: 'auto'}}>
+        <div className="border-radius-med mb-4" style={{overflow: 'auto', height: '100%'}}>
           {checklist.map((group, i) => {
             return <ChecklistGroup key={i} group={group}/>
           })}
+        </div>
+        <AddItemModal groups={checklist}/>
+        <div style={{display: "flex", justifyContent: "end", flexDirection: "row", alignItems: "center"}}>
+          <TealBotton onClick={handleClose} style={{width: '95px'}}>Done</TealBotton>
         </div>
       </Box>
     </Modal>
     </>
   )
+}
+
+function AddItemModal({groups}) {
+  const [open, setOpen] = React.useState(false);
+  const [newItem, setNewItem] = React.useState('');
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return <>
+    <div style={{display: "flex", justifyContent: "center", flexDirection: "row", alignItems: "center"}}>
+      <IconButton sx={{marginLeft: "5px"}} onClick={handleOpen}>
+        <AddCircleIcon sx={{marginLeft: "5px"}} className="color-medium-teal"></AddCircleIcon>
+      </IconButton>
+      <Typography variant='bodyText' className='color-medium-teal' sx={{cursor: 'pointer'}} onClick={handleOpen}>Add Item</Typography>
+    </div>
+    <Modal
+      open={open}
+      onClose={handleClose}
+      scroll='body'
+      aria-labelledby="checklist-dialog"
+    >
+      <Box sx={popUp}>
+        <div style={{display: "flex", justifyContent: "start", flexDirection: "row", alignItems: "center"}}>
+          <IconButton onClick={handleClose}>
+            <ArrowBackIosIcon color='teal' fontSize="small"></ArrowBackIosIcon>
+          </IconButton>
+          <Typography variant='bodyText' className='color-medium-teal' sx={{cursor: 'pointer'}} onClick={handleClose}>Back</Typography>
+        </div>
+        <Typography variant="heading2" className="color-dark-teal" sx={{textAlign: 'center', lineHeight: 0}}>New Item</Typography>
+        <TextField id="outlined-basic" label="I want to do ..." variant="outlined" className="m-4 my-5" value={newItem} onChange={(event) => {setNewItem(event.target.value)}}/>
+        <Typography variant="bodyHeading" className="color-dark-teal" sx={{textAlign: 'center'}}>Add To ...</Typography>
+        <div style={{mt: 3, justifyContent: 'center', display: 'flex', m: 'auto', overflow: 'auto'}}>
+        <FormGroup style={{mt: 3, justifyContent: 'center', display: 'flex', m: 'auto', overflow: 'auto'}}>
+          {groups.map((group, i) => {
+            return <FormControlLabel
+              sx={{display: 'block'}}
+              label={group.name}
+              key={i}
+              control={
+                <Checkbox
+                  icon={<CheckBoxOutlineBlankIcon className="color-medium-teal"/>}
+                  checkedIcon={<CheckBoxIcon className="color-medium-teal"/>}
+                />
+              }
+            />
+          })}
+        </FormGroup>
+        </div>
+        <div style={{display: "flex", justifyContent: "end", flexDirection: "row", alignItems: "center"}}>
+          <TealBotton onClick={handleClose} style={{width: '120px'}}>Add Item</TealBotton>
+        </div>
+      </Box>
+    </Modal>
+  </>
 }
 
 export default ChecklistModal
